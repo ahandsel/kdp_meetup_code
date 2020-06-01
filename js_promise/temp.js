3ca0b630a9efd6711f6716795d7e070101cd0f06
Genji <ahandsel@gmail.com>
2020-06-01 10:58:58 +0900

    Create temp.js

---
+++ js_promise/temp.js
@@ -0,0 +1,20 @@
+function Step_A(clothing) {
+  console.log(clothing);
+}
+
+function Step_B(clothing) {
+  console.log(clothing);
+}
+
+function Step_C(clothing) {
+  console.log(clothing);
+}
+
+Step_A('T-shirt');
+Step_B('Underwear');
+Step_C('Pants');
+
+
+function Step_B(clothing) {
+  getData(Slow_Database);
+}
